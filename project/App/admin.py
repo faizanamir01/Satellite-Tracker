3ae675b9902620406ddd8c9ae9906cddaf80cf95
Faizan Amir <faizanamir2003@gmail.com>
2024-05-06 19:01:37 +0530

    Dockerized the App

--- project/App/admin.py
+++ project/App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# from .models import Satellite,LaunchCountry
+# from import_export import resources
+# from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+# # Register your models here.
+# admin.site.register(Satellite)
+# admin.site.register(LaunchCountry)
+
--- project/App/views.py
+++ project/App/views.py
@@ -1,25 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
+import csv
+from django.http import HttpResponse
+from .models import Satellite, LaunchCountry
 
 
-# def home(request):
-#     # List of satellite names
-#     satellite_names = ['Intelsat', 'Iridium', 'Starlink', 'Orbcomm', 'Swarm', 'SatNogs', 'SES', 'Oneweb', 'globalstar', 'amateur']
-
+# def fetch_satellite_data():
+#     satellite_names = ['intelsat', 'iridium', 'starlink', 'orbcomm', 'swarm', 'satnogs', 'ses', 'oneweb', 'globalstar', 'amateur']
 #     satellite_data = []
 
-#     # Iterate through each satellite name and fetch 30 satellite data entries from each website
 #     for satellite in satellite_names:
 #         url = f'https://celestrak.org/NORAD/elements/gp.php?GROUP={satellite}&FORMAT=json'
 #         response = requests.get(url)
 #         if response.status_code == 200:
-#             satellite_data.extend(response.json()[:70])
+#             satellite_data.extend(response.json()[:30])  # Take 30 entries from each website
 
-#     return render(request, 'home.html', {'satellite_data': satellite_data})
+#     return satellite_data
+
+# def fetch_launch_countries():
+#     satellite_countries = {
+#         'Intelsat': 'USA',
+#         'Iridium': 'USA',
+#         'Starlink': 'USA',
+#         'Orbcomm': 'USA',
+#         'Swarm': 'USA',
+#         'SatNogs': 'USA',
+#         'SES': 'USA',
+#         'Oneweb': 'UK',
+#         'globalstar': 'USA',
+#         'amateur': 'Various',
+#     }
+    
+#     launch_data = [{'satellite_name': name, 'launch_country': country} for name, country in satellite_countries.items() for _ in range(10)]
+
+#     return launch_data
+
+# def home(request):
+#     # Fetch satellite data and save to database
+#     satellite_data = fetch_satellite_data()
+#     for data in satellite_data:
+#         Satellite.objects.create(**data)
+
+#     # Fetch launch country data and save to database
+#     launch_data = fetch_launch_countries()
+#     for data in launch_data:
+#         LaunchCountry.objects.create(**data)
+
+#     # Retrieve data from database
+#     satellites = Satellite.objects.all()[:30]  # Fetching 30 entries
+#     launch_countries = LaunchCountry.objects.all()[:100]  # Fetching 100 entries
+
+#     return render(request, 'home.html', {'satellites': satellites, 'launch_countries': launch_countries})
 
 
-from django.shortcuts import render
-import requests
 
 def home(request):
     # List of satellite names and their launch countries (dummy data)
@@ -48,5 +81,48 @@ def home(request):
 
     # Generating dummy data for the second table
     launch_data = [{'name': name, 'country': country} for name, country in satellite_countries.items()]
+    # return render(request, 'home.html', {'satellite_data': satellite_data, 'launch_data': launch_data})
+
+
+
+
+
+
+    # Generating CSV data
+    csv_data = [['OBJECT_NAME', 'OBJECT_ID', 'EPOCH', 'MEAN_MOTION', 'ECCENTRICITY', 'INCLINATION', 'RA_OF_ASC_NODE', 'ARG_OF_PERICENTER', 'MEAN_ANOMALY', 'EPHEMERIS_TYPE', 'CLASSIFICATION_TYPE', 'NORAD_CAT_ID', 'ELEMENT_SET_NO', 'REV_AT_EPOCH', 'BSTAR', 'MEAN_MOTION_DOT', 'MEAN_MOTION_DDOT']]
+    for entry in satellite_data:
+        csv_data.append([
+            entry['OBJECT_NAME'],
+            entry['OBJECT_ID'],
+            entry['EPOCH'],
+            entry['MEAN_MOTION'],
+            entry['ECCENTRICITY'],
+            entry['INCLINATION'],
+            entry['RA_OF_ASC_NODE'],
+            entry['ARG_OF_PERICENTER'],
+            entry['MEAN_ANOMALY'],
+            entry['EPHEMERIS_TYPE'],
+            entry['CLASSIFICATION_TYPE'],
+            entry['NORAD_CAT_ID'],
+            entry['ELEMENT_SET_NO'],
+            entry['REV_AT_EPOCH'],
+            entry['BSTAR'],
+            entry['MEAN_MOTION_DOT'],
+            entry['MEAN_MOTION_DDOT']
+        ])
+
+    # Create HTTP response with CSV file- Uncomment below lines to download csv on visiting homepage
+    # response = HttpResponse(content_type='text/csv')
+    # response['Content-Disposition'] = 'attachment; filename="satellite_data.csv"'
+    # writer = csv.writer(response)
+    # writer.writerows(csv_data)
+
+    # return response
+
+
     return render(request, 'home.html', {'satellite_data': satellite_data, 'launch_data': launch_data})
+
+
+
+
 
